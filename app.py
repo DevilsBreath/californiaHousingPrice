5c2c3726266cc9aaccab1ddb622e6a277692180c
Shardul Karanjekar <karanjekarshardul3@gmail.com>
2023-09-25 17:04:38 +0530

    Changed app.py and home.html

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
 app = Flask(__name__)
 ## Load the model
 regmodel = pickle.load(open('regmodel.pkl','rb'))
@@ -22,5 +25,14 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route("/predict",methods = ['POST'])
+def predict():
+    data = [ float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html",prediction_text ="The house price prediction is: {}".format(output))
+
+    
 if __name__ == '__main__':
     app.run(debug=True)
