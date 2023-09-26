51cdbe886766b84ee956f52b47287fba26da6b23
Shardul Karanjekar <karanjekarshardul3@gmail.com>
2023-09-26 11:03:21 +0530

    Corrected the error in app.py file.

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
 
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-
 app = Flask(__name__)
 ## Load the model
 regmodel = pickle.load(open('regmodel.pkl','rb'))
@@ -28,11 +25,11 @@ def predict_api():
 @app.route("/predict",methods = ['POST'])
 def predict():
     data = [ float(x) for x in request.form.values()]
-    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    final_input = scaling.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output = regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text ="The house price prediction is: {}".format(output))
 
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
